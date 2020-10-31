b15610af52beb0d6098cf5f2a4de1bad26fade38
mbw10 <mehmet.batman@centria.fi>
2020-10-31 17:41:44 +0200

    Lisäsin framgmenttiä(SearchTask,Createtask,mytasks)

---
+++ app/src/main/java/com/example/housework/ui/task/CreateTask.java
@@ -0,0 +1,38 @@
+package com.example.housework.ui.task;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.housework.R;
+
+public class CreateTask extends Fragment {
+
+    private CreateTaskViewModel mViewModel;
+
+    public static CreateTask newInstance() {
+        return new CreateTask();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.create_task_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(CreateTaskViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/housework/ui/task/SearchTaskViewModel.java
@@ -0,0 +1,7 @@
+package com.example.housework.ui.task;
+
+import androidx.lifecycle.ViewModel;
+
+public class SearchTaskViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
