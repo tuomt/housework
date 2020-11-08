f1aee330c5994ae10ee033170de3d851abed0a15
mbw10 <mehmet.batman@centria.fi>
2020-11-08 18:54:04 +0200

    Add two fragments and a working button from SearchTask to SearchResult

--- app/src/main/java/com/example/housework/ui/task/SearchTask.java
+++ app/src/main/java/com/example/housework/ui/task/SearchTask.java
@@ -1,21 +1,21 @@
 package com.example.housework.ui.task;
 
-import androidx.lifecycle.ViewModelProviders;
-
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.ViewModelProviders;
 
 import com.example.housework.R;
 
 public class SearchTask extends Fragment {
-
+Button btnetsi1;
     private SearchTaskViewModel mViewModel;
 
     public static SearchTask newInstance() {
@@ -25,7 +25,29 @@ public class SearchTask extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search_tasks, container, false);
+
+
+
+     View v=   inflater.inflate(R.layout.fragment_search_tasks, container, false);
+   btnetsi1=v.findViewById(R.id.btnetsi);
+   btnetsi1.setOnClickListener(new View.OnClickListener() {
+       @Override
+       public void onClick(View v) {
+           SearchResults searchResults=new SearchResults();
+
+           FragmentTransaction transaction;
+           transaction = getFragmentManager().beginTransaction();
+           transaction.replace(R.id.drawer_layout,searchResults);
+           FragmentTransaction transaction1 = transaction.addToBackStack(null);
+           int commit = transaction.commit();
+
+       }
+   });
+
+     return v;
+
+
+
     }
 
     @Override
