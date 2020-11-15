cfe93c6995a47ee8903f91da8b55cb361035fe6c
AaroP <aaro.piirainen@centria.fi>
2020-11-15 17:45:46 +0200

    Base for new branch idea for this branch is to fix mainactivitys menu/navigation

--- app/src/main/java/com/example/housework/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/housework/ui/home/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,6 +12,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.Navigation;
 
 import com.example.housework.R;
 
