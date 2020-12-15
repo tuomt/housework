7aa0d28aea3badcc6e343ac50f98bb298b83b969
tuomt <46566906+tuomt@users.noreply.github.com>
2020-12-16 01:16:24 +0200

    Add file

---
+++ app/src/main/java/com/example/housework/api/InputValidator.java
@@ -0,0 +1,34 @@
+package com.example.housework.api;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class InputValidator {
+
+    public static boolean isValidPassword(final String password) {
+        int len = password.length();
+        if (len < 8 || len > 85) {
+            return false;
+        } else return true;
+    }
+
+    public static boolean isValidUserName(final String userName) {
+        int len = userName.length();
+        if (len < 2 || len > 21) {
+            return false;
+        } else return true;
+    }
+
+    public static boolean isValidEmail(final String email) {
+        int len = email.length();
+        if (len < 5 || len > 85) {
+            return false;
+        }
+
+        Pattern pattern = Pattern.compile("^.+@.+(\\.[^.]+)+$");
+        Matcher matcher = pattern.matcher(email);
+        if (matcher.matches()) {
+            return true;
+        } else return false;
+    }
+}
